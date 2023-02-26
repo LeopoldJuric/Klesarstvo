03072a9e08a62e2804fb570fafde1f835d632b4f
LeopoldJuric <leopoldjurickc@gmail.com>
2023-02-26 12:01:04 +0100

    fix

--- components/Navbarpodstr.jsx
+++ components/Navbarpodstr.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
+import logoimg from '../public/images/logo.png'
+import Image from 'next/image'
 
 const Navbarpodstr = ({podstr}) => {
 
@@ -7,7 +9,7 @@ const Navbarpodstr = ({podstr}) => {
     <div className='fixed left-0 top-0 w-full text-center bg-black'>
         <div className='flex flex-col max-w-[1240px] m-auto p-4 text-white md:flex-row'>
             <Link href='/' className='w-[200px] bg-white pt-2 m-auto mb-3 md:m-0'>
-                <img src='../images/logo.png' className='p-4' alt=''/>
+                <Image src={logoimg} className='p-4' alt=''/>
             </Link>
             <div className='border-2 h-[100] ml-5'></div>
             <h1 className='text-5xl mt-2 md:p-4 md:mt-0'>
--- components/Slider.jsx
+++ components/Slider.jsx
@@ -3,44 +3,33 @@ import React, { useState } from 'react'
 import { SliderData } from './SliderData'
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
 import Link from 'next/link'
+import SimpleImageSlider from 'react-simple-image-slider'
 
-function Slider({slides}) {
+function Slider() {
 
-    const [current, setCurrent] = useState(0)
-    const length = slides.length
-
-    const nextSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1)
-    }
-
-    const prevSlide = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1)
-    }
-
-    if (!Array.isArray(slides) || slides.length <= 0) {
-        return null;
-    }
+    const images = [
+        { url: "/images/gallery/IMG-20230121-WA0004.jpg" },
+        { url: "/images/gallery/IMG-20230121-WA0006.jpg" },
+        { url: "/images/gallery/IMG-20230121-WA0007.jpg" },
+        { url: "/images/gallery/IMG-20230121-WA0008.jpg" },
+        { url: "/images/gallery/IMG-20230121-WA0009.jpg" },
+      ];
 
 
   return (
     <div id='gallery' className='max-w-[1240px] mx-auto py-2'>
         <h1 className='text-2xl font-bold text-center p-8'>Galerija</h1>
         <div className='relative flex justify-center p-4'>
-        {SliderData.map((slide, index) => {
-            return (
-                <div key={index} className={index === current ? 'opacity-[1] ease-in duration-1000' : 'opacity-0'}>
-                    <FaArrowCircleLeft size={50} 
-                        onClick={prevSlide}
-                        className='absolute top-[50%] left-[5%] cursor-pointer select-none z-[2]' />
-                    {index === current && (
-                        <Image src={slide.image} alt='/' width='1440' height='300' className='w-full h-[700px]'/>
-                    )}
-                    <FaArrowCircleRight size={50}
-                        onClick={nextSlide} 
-                        className='absolute top-[50%] right-[5%] cursor-pointer select-none z-[2]' />
-                </div>
-            )
-        })}
+            <SimpleImageSlider
+                width={896}
+                height={504}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+                navStyle={2}
+                autoPlay={true}
+                autoPlayDelay={3.5}
+            />
         </div>
             <button className='pattern text-white m-auto flex p-4 rounded duration-300 hover:text-white/50'>
                 <Link href='/galerija'>
